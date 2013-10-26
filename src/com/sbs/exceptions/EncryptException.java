a5847efa945d4016594c2a66abbdd474a31ecb02
Kushal <kushaldv@gmail.com>
2013-10-26 13:33:58 -0700

    Exceptions

---
+++ src/com/sbs/exceptions/EncryptException.java
@@ -0,0 +1,15 @@
+package com.sbs.exceptions;
+
+public class EncryptException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public EncryptException(String  message) {
+		super(message);
+	}
+	
+	public String getMessage() {
+		return super.getMessage();
+	}
+
+}
---
+++ src/com/sbs/exceptions/KeyPairGeneratorException.java
@@ -0,0 +1,15 @@
+package com.sbs.exceptions;
+
+public class KeyPairGeneratorException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public KeyPairGeneratorException(String  message) {
+		super(message);
+	}
+	
+	public String getMessage() {
+		return super.getMessage();
+	}
+
+}
